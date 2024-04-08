c3349e48318f3ed9eac6f8f3f7c1f3581945a2fe
raphaelrealina <155679935+raphaelrealina@users.noreply.github.com>
2024-04-07 22:27:13 -0500

    adding initial files

---
+++ app/src/main/java/com/example/realwordwizard/GameActivity.java
@@ -0,0 +1,25 @@
+package com.example.realwordwizard;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.game), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setContentView(R.layout.activity_game);
+        Intent i = getIntent();
+    }
+}
